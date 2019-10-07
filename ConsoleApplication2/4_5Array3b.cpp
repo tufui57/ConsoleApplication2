3e22be0c55eaab1de96caee0dae645038dac8fba
Miki Nomura <nomura_jpngm@live.jp>
2019-10-07 18:04:29 +1300

    added new codes

--- ConsoleApplication2/4_5Array3b.cpp
+++ ConsoleApplication2/4_5Array3b.cpp
@@ -2,4 +2,17 @@
 
 using namespace std;
 
+void showAddress(char* pointer) {
+	cout << (size_t)pointer << endl;
+	for (int i = 0; i < 10; ++i) {
+		cout << (size_t)&pointer[i] << endl;
+	}
+}
 
+int main() {
+	char array[7];
+	cout << (size_t)array << endl;
+
+	cout << endl;
+	showAddress(array);
+}
