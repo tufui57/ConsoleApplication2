d049b0c9c48144c94a6c45f4a5729d74e343f34b
DESKTOP-3U6270B\nomur <nommi038@student.otago.ac.nz>
2019-10-08 16:41:14 +1300

    added codes

--- ConsoleApplication2/4_5Array3.cpp
+++ ConsoleApplication2/4_5Array3.cpp
@@ -3,18 +3,32 @@
 using namespace std;
 
 
+//void show(char array[]) {
+//	int i = 0;
+//	while (i <= sizeof(array)) {
+//		cout << (size_t)&array[i] << " ";
+//		++i;
+//	}
+//	cout << endl;
+//}
+//
+//// whatever number I give as the number of array s, the output shows 5 elements. why? 
+//int main() {
+//	char s[2];
+//	show(s);
+//
+//}
+
+
 void show(char array[]) {
-	int i = 0;
-	while (i <= sizeof(array)) {
-		cout << (size_t)&array[i] << " ";
-		++i;
+	for (int i = 0; i < 4; ++i) {
+		cout << (size_t)&array[i] << endl;
 	}
-	cout << endl;
 }
 
-// whatever number I give as the number of array s, the output shows 5 elements. why? 
+// Why the number of output is always "i"? The number of elements in the array s is 2, though.
 int main() {
-	char s[12];
+	char s[2];
 	show(s);
 
 }
--- ConsoleApplication2/4_5Array3b.cpp
+++ ConsoleApplication2/4_5Array3b.cpp
@@ -2,4 +2,22 @@
 
 using namespace std;
 
+void show(char array[]) {
+	for (int i = 0; i < 10; ++i) {
+		cout << (size_t)&array[i] << endl;
+	}
+}
 
+void showaddress(char* pointer) {
+	cout << "pointer: " << (size_t)pointer << endl;
+	for (int i = 0; i < 10; ++i) {
+		cout << "&pointer: " << (size_t)&pointer[i] << endl;
+	}
+	
+}
+
+int main() {
+	char s[2];
+	show(s);
+	showaddress(s);
+}
