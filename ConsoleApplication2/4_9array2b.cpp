d733d3255488f58052c41d0c44958c797842c3b1
tufui57 <nomura_jpngm@live.jp>
2019-10-28 15:00:37 +1300

    add a file

--- ConsoleApplication2/4_9array2b.cpp
+++ ConsoleApplication2/4_9array2b.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int strCount(const char* str, char ch) {
-	
+// Show multiples of "x" with the size is "array_size".
+
+const int array_size = 4; // array_size shouldn't be changed anywhere. So, it's declared as const.
+
+void Init(int array[]) {
+	int x = 2;
+	for (int i = 0; i < array_size; ++i) {
+		array[i] = i * x;
+	}
+}
+
+void show(int array[]) {
+	for (int i = 0; i < array_size; ++i) {
+		cout << array[i] << ' ';
+	}
+	cout << endl;
 }
 
 int main() {
+	int n[array_size];
+	Init(n);
+	show(n);
 }
